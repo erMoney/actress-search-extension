055a5b18f039c32bfb20ba8e61324e079379ec90
shunta_ichikawa <shunta@ichikawa.local>
2017-11-05 15:00:45 +0900

    fix some code

--- src/scripts/resultsScripts.js
+++ src/scripts/resultsScripts.js
@@ -12,11 +12,7 @@ chrome.runtime.getBackgroundPage((backgroundPage) => {
 });
 
 const displayName = (name) => {
-    let temp = `
-        <span class="mdl-layout-title">
-            <div style="font-family: 'Noto Serif', serif;">${name}</div>
-        </span>
-    `;
+    let temp = `Actress search results ... ${name}`;
     $('.actress-name').append(temp);
     $('.actress-name').show('slow');
 };
@@ -39,37 +35,26 @@ const createAvgleLink = (name) => {
         let $videoElement = $('.actress-video')
         let count = 1;
         const MAX = videos.length;
-        let temp = '';
+        let temp = '<div class="mdl-grid">';
         for(let video of videos) {
-            if( ( count % 2 ) != 0 ) {
-                temp += `
-                    <div class="mdl-grid">
-                        <div class="mdl-layout-spacer"></div>
-                    `;
-            }
             temp += `
-                        <div class="card-square mdl-cell mdl-card mdl-shadow--2dp">
-                          <div class="mdl-card__title mdl-card--expand" style="background: url(${video.preview_url}) top right / 100% 100% no-repeat">
-                            <h2 class="mdl-card__title-text"></h2>
-                          </div>
-                          <div class="mdl-card__supporting-text">
-                            ${video.title}
-                          </div>
-                          <div class="mdl-card__actions mdl-card--border">
-                            <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="${video.video_url}" target="_blank">
-                              動画を見る
-                            </a>
-                          </div>
-                        </div>
+                <div class="card-square mdl-cell--6-col mdl-cell mdl-card mdl-shadow--2dp">
+                  <div class="mdl-card__title mdl-card--expand" style="background: url(${video.preview_url}) top right / 100% 100% no-repeat">
+                    <h2 class="mdl-card__title-text"></h2>
+                  </div>
+                  <div class="mdl-card__supporting-text">
+                    ${video.title}
+                  </div>
+                  <div class="mdl-card__actions mdl-card--border">
+                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="${video.video_url}" target="_blank">
+                      動画を見る
+                    </a>
+                  </div>
+                </div>
             `;
-            if (( count % 2 ) == 0 || count == MAX) {
-                temp += `
-                        <div class="mdl-layout-spacer"></div>
-                    </div>
-                `;
-            }
             count++;
         }
+        temp += '</div>';
         $videoElement.append(temp);
         $videoElement.show('slow');
     };
