fb3d647088fc733db8ff53ded9c50b30b7f8c991
Noda Shimpei <noda.sin@gmail.com>
2017-11-10 21:42:43 +0900

    Promisified. Draw image on console log

--- src/scripts/backgroundScripts.js
+++ src/scripts/backgroundScripts.js
@@ -3,109 +3,91 @@ import $ from 'jquery';
 import 'tracking/build/data/face';
 
 let popupResults = {};
-let isDetect = false;
 const apiEndpoint = 'https://actress-search.herokuapp.com/face:recognition';
 
-function cropData(str, coords, tabId, callback)
+function consoleImage(src)
 {
-    let img = new Image();
+    console.log('%c       ', 'font-size: 100px; background: url('+src+') no-repeat; background-size:contain;');
+}
 
-    img.onload = () => {
-        if (isDetect) return;
+function promiseImage(src)
+{
+    return new Promise((resolve) => {
+        let img = new Image();
+        img.onload = () => {
+            resolve(img);
+        };
+        img.src = src;
+    });
+}
 
+function cropImage(src, coords)
+{
+    return promiseImage(src)
+    .then((img) => {
         let canvasObj = document.createElement('canvas');
         canvasObj.width = coords.w;
         canvasObj.height = coords.h;
 
         let ctx = canvasObj.getContext('2d');
         ctx.drawImage(img, coords.x, coords.y, coords.w, coords.h, 0, 0, coords.w, coords.h);
+        return canvasObj.toDataURL();
+    });
+}
 
-        // create base64 image
-        let base64img = canvasObj.toDataURL();
+function faceDetect(src)
+{
+    console.log('faceDetect');
+    consoleImage(src);
 
-        // create image element
-        let bodyObj = document.getElementsByTagName('body').item(0);
-        let cropImgObj = document.createElement('img');
-        cropImgObj.setAttribute('id','trackingImage');
-        cropImgObj.width = coords.w;
-        cropImgObj.height = coords.h;
-        cropImgObj.src = base64img;
-        bodyObj.appendChild(cropImgObj);
+    return promiseImage(src)
+    .then((img) => {
+        $('body').append(img);
+        $(img).attr('id', 'trackingImage');
 
-        const cropImgOnload = () => {
-            return new Promise((resolve) => {
-                cropImgObj.onload = () => {
-                    resolve();
+        const tracker = new tracking.ObjectTracker('face');
+        tracker.setInitialScale(4);
+        tracker.setStepSize(2);
+        tracker.setEdgesDensity(0.1);
+        tracking.track('#trackingImage', tracker);
+
+        return new Promise((resolve, reject) => {
+            tracker.on('track', (event) => {
+                console.log(event);
+                $(img).remove();
+
+                if(event.data.length == 0) {
+                    reject(new Error('Face is not detected.'));
+                    return;
                 }
+                resolve(img.src);
             });
-        }
+        });
+    });
+}
 
-        const trackingImage = () => {
-            return new Promise((resolve, reject) => {
-                // detect init
-                const tracker = new tracking.ObjectTracker('face');
-                tracker.setInitialScale(4);
-                tracker.setStepSize(2);
-                tracker.setEdgesDensity(0.1);
-                tracking.track('#trackingImage', tracker);
-                // detect start
-                tracker.on('track', (event) => {
-                    console.log(event);
-                    // clear image element
-                    if (document.querySelector('#trackingImage') != null) {
-                        document.querySelector('#trackingImage').remove();
-                    }
-                    if(event.data.length == 0) {
-                        reject('not detection.');
-                        return;
-                    }
-                    isDetect = true;
-                    resolve();
-                });
-            });
-        }
-
-        const faceRecognition = () => {
-            return new Promise((resolve, reject) => {
-                let body = {
-                    'image': base64img.replace(/^.*,/, '')
-                };
-                $.ajax({
-                    type: 'POST',
-                    url: apiEndpoint,
-                    contentType: 'application/json',
-                    cache: false,
-                    data: JSON.stringify(body),
-                    dataType: 'json'
-                })
-                    .done((response, textStatus, jqXHR) => {
-                        console.log(response);
-                        resolve(response);
-                    })
-                    .fail((jqXHR, textStatus, errorThrown) => {
-                        console.log(jqXHR);
-                        reject('not found.');
-                    });
-            });
-        }
-
-        cropImgOnload()
-            .then(trackingImage)
-            .then(faceRecognition)
-            .then((res) => {
-                // set popupResults
-                popupResults[tabId] = res.face;
-                popupResults.id = tabId
-                window.popupResults = popupResults;
-                // createPopUp
-                callback(res);
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-    };
-
-    img.src = str;
+function faceRecognize(src)
+{
+    return new Promise((resolve, reject) => {
+        let body = {
+            'image': src.replace(/^.*,/, '')
+        };
+        $.ajax({
+            type: 'POST',
+            url: apiEndpoint,
+            contentType: 'application/json',
+            cache: false,
+            data: JSON.stringify(body),
+            dataType: 'json'
+        })
+        .done((res, textStatus, jqXHR) => {
+            console.log(res);
+            resolve(res.face);
+        })
+        .fail((jqXHR, textStatus, errorThrown) => {
+            reject(new Error('Face is not recognized.'));
+        });
+    });
 }
 
 chrome.browserAction.onClicked.addListener(() => {});
@@ -118,18 +100,32 @@ function capture(coords, tabId)
         count++;
     }
     chrome.tabs.captureVisibleTab(null, { format: 'png' }, (data) => {
-        const id = setInterval(() => {
+        const run = () => {
             console.log(count);
-            if(isDetect || count > 10) {　
-                clearInterval(id);
+            if(count > 10) {　
+                return;
             }
             countUp();
-            cropData(data, coords, tabId, createPopUp);
-        }, 1000);
+
+            cropImage(data, coords)
+            .then(faceDetect)
+            .then(faceRecognize)
+            .then(function (face) {
+                console.log("Success to detect actress", face);
+                popupResults[tabId] = face;
+                popupResults.id = tabId
+                window.popupResults = popupResults;
+                createPopUp();
+            }).catch((err) => {
+                console.log(err);
+                setTimeout(run, 1000);
+            });
+        };
+        run();
     });
 }
 
-function createPopUp(response)
+function createPopUp()
 {
     const popup_options = {
         url: 'html/results.html',
